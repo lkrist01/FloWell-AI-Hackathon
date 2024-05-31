0fdd37c67c770d5a332c4d2731a3032327e5b64f
lkrist01 <kristilici@gmail.com>
2024-05-31 17:41:16 +0100

    Update chatbot to have user/bot sessions

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import random
 import altair as alt
 import numpy as np
 import pandas as pd
@@ -415,14 +416,45 @@ else:
         else:
             st.warning('Complete the remaining task to proceed discharge!', icon="⚠️")
 
+#********************************** Chatbot section in sidebar****************************************************
+# Streamed response emulator
+def response_generator():
+    response = random.choice(
+        [
+            "Hello there! How can I assist you today?",
+            "Hi, human! Is there anything I can help you with?",
+            "Do you need help?",
+        ]
+    )
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
+
 with st.sidebar:
     with st.expander("Chat Bot:", expanded=True):
-        messages = st.container(height=300)
-        messages.chat_message("user").write("Hello!")
-        messages.chat_message("assistant").write(f"Echo: Hello, how can i help you?")
+        messages = st.container(height=350, )
 
-        if prompt := st.chat_input("Say something"):
-            messages.chat_message("user").write(prompt)
-            messages.chat_message("assistant").write(f"Echo: {prompt}")
+        # Initialize chat history
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
+
+        # Display chat messages from history on app rerun
+        for message in st.session_state.messages:
+            with messages.chat_message(message["role"]):
+                messages.markdown(message["content"])
+
+        # Accept user input
+        if prompt := st.chat_input("Ask something?"):
+            # Add user message to chat history
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            # Display user message in chat message container
+            with messages.chat_message("user"):
+                messages.markdown(prompt)
+
+            # Display assistant response in chat message container
+            with st.chat_message("assistant"):
+                response = messages.write_stream(response_generator())
+                # Add assistant response to chat history
+                st.session_state.messages.append({"role": "assistant", "content": response})
 
 
